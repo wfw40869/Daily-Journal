9577cc1efd9746231eadff12dae33aed2b6931bb
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-12 14:38:43 -0500

    Added journal.js file

---
+++ src/scripts/journal.js
@@ -0,0 +1,30 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntries = [
+    {
+        date: "07/09/2019",
+        conceptsCovered: "Group Projects",
+        entryMessage: "Learned a lot about working in a group. Plan, Plan Plan. Have clear and concise goals. Also, have a reach goal and a minimum viable product goal.",
+        mood: "happy"
+    },
+    {
+        date: "07/10/2019",
+        conceptsCovered: "Using the Debugger",
+        entryMessage: "The debugger is very useful. Creating a flag to pause code to see what something is at that point in time is awesome!",
+        mood: "happy"
+    },
+    {
+        date: "07/11/2019",
+        conceptsCovered: "Manipulating the DOM",
+        entryMessage: "Learned about foreach loops, navigating through objects and looping through arrays",
+        mood: "happy"
+    },
+    {
+        date: "07/12/2019",
+        conceptsCovered: "Manipulating the DOM and Objects",
+        entryMessage: "Learned about foreach loops, changing elements' classes using JS DOM manipulation, navigating through objects and looping through arrays",
+        mood: "happy"
+    }
+]
